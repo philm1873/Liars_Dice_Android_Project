aa73ae1ea1d06c44525868f44396e18cca60e435
Phil Mitchell <pm1873@gmail.com>
2018-01-11 23:46:08 +0000

    Re-factor

--- app/src/main/java/com/example/codeclan/liarsdice/GameActivity.java
+++ app/src/main/java/com/example/codeclan/liarsdice/GameActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
